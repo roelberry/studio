ef3cb60f96f0e34e375b16ee2f38cd0120842ba6
Rochelle Berry <rochelleberry731@gmail.com>
2025-11-09 09:43:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8522178126-1c4ba",
+  "appId": "1:1017820369346:web:339bb6f5e7f300435f865b",
+  "apiKey": "AIzaSyBDZLcI66ZFr-0n3omlm9NUhybvwaVaeck",
+  "authDomain": "studio-8522178126-1c4ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1017820369346"
+};
